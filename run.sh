bcf24062e07d044bb5b08a33602cf6278aebc50a
karinvk <75458512+karinvk@users.noreply.github.com>
2024-02-14 11:08:58 +0000

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-python ./datasets/dataloader.py 
---dataset CIFAR10
+python ./datasets/dataloader.py \
+--dataset CIFAR10 \
 --batch_size 50
 
 python ./models/model.py
 
-python ./train.py
+python ./train.py \
 --configs.yaml
