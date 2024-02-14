c2ab760280437ab51475b2309ce67ce6212c26cb
karinvk <75458512+karinvk@users.noreply.github.com>
2024-02-14 15:24:12 +0100

    Update dataloader.py

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -3,19 +3,9 @@ from torch.utils.data import DataLoader
 import argparse
 
 def data_loader(dataset,batch_size):
-    train_data = torchvision.datasets.dataset('../data', train=True, transform=torchvision.transforms.ToTensor(), download=True)
-    train_loder = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    test_data = torchvision.datasets.dataset('../data', train=False, transform=torchvision.transforms.ToTensor(), download=True)
+    dataset_func = getattr(torchvision.datasets, dataset)
+    train_data = dataset_func('./', train=True, transform=torchvision.transforms.ToTensor(), download=True)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_data = dataset_func('./', train=False, transform=torchvision.transforms.ToTensor(), download=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
-
-def setup_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=100, help="batch size")
-    parser.add_argument("--dataset", type=str, default=None, help="dataset")
-    return parser
-
-
-if __name__ == "__main__":
-    parser = setup_parser()
-    args = parser.parse_args()
-    data_loader(args)
+    return train_data,test_data,train_loader,test_loader
